21102bd0b845e4d43ded05ef1856cccb6c80f3d0
Murilo Araujo <muernandes@icloud.com>
2019-11-26 00:09:45 -0300

    created main.js file

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
+
+app.on("ready", function() {
+  //Create new window
+  mainWindow = new BrowserWindow({});
+  // Load html file
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+});
